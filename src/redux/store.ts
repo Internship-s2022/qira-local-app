b38a90b2185ef5cd0c1d144cad5e0627c70b96e7
FranGutierrez <francisco.gutierrez@radiumrocket.com>
2022-09-23 10:57:20 -0300

    QL-14: Install Redux Devtools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({});
 
 const configureStore = () => {
-  return createStore(rootReducer);
+  const enhacer = composeWithDevTools();
+  return createStore(rootReducer, enhacer);
 };
 
 const store = configureStore();
