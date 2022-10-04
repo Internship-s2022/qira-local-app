f55d5404e13176f667543917c95ecb7ea5096108
Gina Schiappa Pietra <gina.schiappapietra@radiumrocket.com>
2022-10-04 16:34:21 -0300

    QL-30: override styles

--- src/components/shared/modal/index.tsx
+++ src/components/shared/modal/index.tsx
@@ -3,30 +3,17 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Typography from '@mui/material/Typography';
 
+import style from './modal.module.css';
 import { SharedModalProps } from './types';
 
 export const SharedModal = (props: SharedModalProps): JSX.Element => {
-  const style = {
-    position: 'absolute' as const,
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
-
   return (
     <Modal open={props.open} onClose={props.onClose}>
-      <Box sx={style}>
+      <Box className={style.container}>
         <Typography id="modal-modal-title" variant="h6" component="h2">
           {props.title}
         </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          {props.message}
-        </Typography>
+        <Typography id="modal-modal-description">{props.message}</Typography>
       </Box>
     </Modal>
   );
