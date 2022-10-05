9221445526c90c6916042853edf02564eb9212ba
Gina Schiappa Pietra <gina.schiappapietra@radiumrocket.com>
2022-10-05 13:17:58 -0300

    QL-79: add select to form

--- src/components/pages/storybook/index.tsx
+++ src/components/pages/storybook/index.tsx
@@ -9,41 +9,59 @@ import InputAdornment from '@mui/material/InputAdornment';
 
 import { InputText } from 'src/components/shared/input';
 import { SharedModal } from 'src/components/shared/modal';
+import { SharedSelect } from 'src/components/shared/select';
+import { Options } from 'src/components/shared/select/types';
 import List from 'src/components/shared/ui/list';
 import { Headers, TableButton } from 'src/components/shared/ui/list/types';
 
-import styles from './test.module.css';
+import styles from './storybook.module.css';
 import { TestCompValues } from './types';
 
 const schema = Joi.object({
   firstName: Joi.string().required(),
   email: Joi.string().required(),
   password: Joi.string().required(),
+  select: Joi.string().required(),
 });
 
 interface Admin {
   id: string;
   name: string;
   email: string;
+  value: string;
   isActive: boolean;
 }
-
 const Storybook = (): JSX.Element => {
+  const options: Options[] = [
+    {
+      label: 'Valor 1',
+      value: 'Valor 1',
+    },
+    {
+      label: 'Valor 2',
+      value: 'Valor 2',
+    },
+    {
+      label: 'Valor 3',
+      value: 'Valor 3',
+    },
+  ];
   const { handleSubmit, control, reset } = useForm<TestCompValues>({
     defaultValues: {
       firstName: '',
       email: '',
       password: '',
+      select: '',
     },
     mode: 'onSubmit',
     resolver: joiResolver(schema),
   });
 
   const data: Admin[] = [
-    { id: '1', name: 'Fran', email: 'fran@gmail.com', isActive: true },
-    { id: '2', name: 'Gina', email: 'gina@gmail.com', isActive: false },
-    { id: '3', name: 'Chaki', email: 'chaki@gmail.com', isActive: true },
-    { id: '4', name: 'Ari', email: 'ari@gmail.com', isActive: false },
+    { id: '1', name: 'Francisco', email: 'francisco@gmail.com', value: 'Valor 2', isActive: true },
+    { id: '2', name: 'Gina', email: 'gina@gmail.com', value: 'Valor 3', isActive: false },
+    { id: '3', name: 'Ivan', email: 'ivan@gmail.com', value: 'Valor 3', isActive: true },
+    { id: '4', name: 'Ariana', email: 'ariana@gmail.com', value: 'Valor 1', isActive: false },
   ];
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
@@ -52,6 +70,7 @@ const Storybook = (): JSX.Element => {
   const headers: Headers[] = [
     { header: 'Name', key: 'name' },
     { header: 'Email', key: 'email' },
+    { header: 'Value', key: 'value' },
   ];
 
   const buttons: ((rowData?: Admin) => TableButton)[] = [
@@ -68,6 +87,7 @@ const Storybook = (): JSX.Element => {
       id: '01',
       name: data.firstName,
       email: data.email,
+      value: data.select,
       isActive: true,
     };
     listData.push(newAdmin);
@@ -102,24 +122,31 @@ const Storybook = (): JSX.Element => {
               type="password"
               optionalLabel="Password"
               variant="outlined"
-              margin="normal"
+              margin="dense"
               size="small"
               fullWidth={true}
             />
             <InputText
               control={control}
               name="firstName"
-              label="First name"
               variant="outlined"
+              optionalLabel="Name"
               margin="dense"
               size="small"
               fullWidth={true}
             />
-            <Button onClick={() => reset()} variant="outlined" className={styles.button}>
-              Reset
-            </Button>
+            <SharedSelect
+              label="Select"
+              options={options}
+              name="select"
+              control={control}
+              fullWidth
+            />
             <Button onClick={handleSubmit(onSubmit)} variant="contained" className={styles.button}>
               Submit
+            </Button>
+            <Button onClick={() => reset()} variant="outlined" className={styles.button}>
+              Reset
             </Button>
           </div>
         </form>
