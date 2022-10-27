226ec5c1d1b78c851bf46a9b201b73603454082e
FranGutierrez <francisco.gutierrez@radiumrocket.com>
2022-10-27 12:21:38 -0300

    QL-97: request changes done

--- src/components/pages/admin/category/types.ts
+++ src/components/pages/admin/category/types.ts
@@ -12,11 +12,3 @@ export interface ImageToSend {
   type: string;
   isNew: boolean;
 }
-
-export const toBase64 = (file: any) =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = (error) => reject(error);
-  });
