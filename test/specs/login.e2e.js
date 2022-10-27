29e48c556523cb36680c0cb869b1ce3bb203a33a
Ariana Maldonado <ariana.maldonado@radiumrocket.com>
2022-10-27 17:18:11 -0300

    TG-231: Started with login spec

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,5 +1,11 @@
 import LoginPage from  '../page-objects/login.page';
 
+describe('Login page testing', () => {
+  beforeAll('Open browser', () => {
+    LoginPage.openBrowser();
+  });
+  beforeEach('Refresh the page', () => {
+    browser.refresh();
+  });
 
-
-
+});
