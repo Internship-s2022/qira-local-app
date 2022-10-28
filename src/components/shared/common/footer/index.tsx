e231c8b3d61964d5f15e9a4de4bab50adeae26e2
Gina Schiappa Pietra <gina.schiappapietra@radiumrocket.com>
2022-10-28 12:36:51 -0300

    QL-172: footers styles

--- src/components/shared/common/footer/index.tsx
+++ src/components/shared/common/footer/index.tsx
@@ -5,28 +5,44 @@ import styles from './footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.container} data-testId="test">
-      <div className={styles.main}>
-        <div className={styles.appName}>Frontend Template</div>
+      <div className={styles.logoContainer}>
+        <img
+          className={styles.logo}
+          src={`${process.env.PUBLIC_URL}/assets/images/logo-qira.png`}
+        />
+        <span className={styles.license}>© 2022 QIRA. Todos los derechos reservados.</span>
       </div>
-      <div className={styles.license}>
-        <div className={styles.copyright}>Copyright © 2022 Radium Rocket</div>
-        <div>
-          <a href="https://www.facebook.com/radiumrocket" target="_blank" rel="noreferrer">
+      <div className={styles.socialMediaContainer}>
+        <span className={styles.titelSocialMedia}>Seguinos en nuestras redes</span>
+        <div className={styles.socialMedia}>
+          <a href="https://www.facebook.com/qira-local" target="_blank" rel="noreferrer">
             <img
               className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/facebook.svg`}
+              src={`${process.env.PUBLIC_URL}/assets/images/social-media/facebook-icon.png`}
+            />
+          </a>
+          <a href="https://www.instagram.com/qira-local" target="_blank" rel="noreferrer">
+            <img
+              className={styles.socialIcon}
+              src={`${process.env.PUBLIC_URL}/assets/images/social-media/instagram-icon.png`}
             />
           </a>
           <a href="https://twitter.com/radiumrocket" target="_blank" rel="noreferrer">
             <img
               className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/twitter.svg`}
+              src={`${process.env.PUBLIC_URL}/assets/images/social-media/twitter-icon.png`}
             />
           </a>
-          <a href="https://www.instagram.com/radium.rocket/" target="_blank" rel="noreferrer">
+          <a href="https://www.linkedin.com/qira-local" target="_blank" rel="noreferrer">
             <img
               className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/instagram.svg`}
+              src={`${process.env.PUBLIC_URL}/assets/images/social-media/linkedin-icon.png`}
+            />
+          </a>
+          <a href="#" target="_blank" rel="noreferrer">
+            <img
+              className={styles.socialIcon}
+              src={`${process.env.PUBLIC_URL}/assets/images/social-media/whatsapp-icon.png`}
             />
           </a>
         </div>
