50f979c27ae431c0b9ab656ed7a425c6b203f86d
FranGutierrez <francisco.gutierrez@radiumrocket.com>
2022-11-01 17:51:04 -0300

    QL-170: fixed some styles

--- src/components/pages/client/search-results/index.tsx
+++ src/components/pages/client/search-results/index.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import ProductCard from 'src/components/shared/ui/product-card';
 import { getProductsFilteredBySearch } from 'src/redux/products/selectors/getProductsBySearch';
 import { getPublicProducts } from 'src/redux/products/thunks';
 import { AppDispatch, RootState } from 'src/redux/store';
+import { capitalizeFirstLetter } from 'src/utils/formatters';
 
 import styles from './search-results.module.css';
 
@@ -22,18 +23,27 @@ export const SearchProductsList = (): JSX.Element => {
   }, []);
 
   return (
-    <section className={styles.productsContainer}>
-      {filteredProducts.length > 0 ? (
-        filteredProducts.map((product) => {
-          return <ProductCard key={product._id} product={product} />;
-        })
-      ) : (
-        <div className={styles.messageContainer}>
-          <h2 className={styles.message}>
-            Lo sentimos, no se encontraron productos que coincidan con su búsqueda.
-          </h2>
-        </div>
-      )}
-    </section>
+    <div className={styles.mainContainer}>
+      <div className={styles.navHistory}>
+        <Link className={styles.navLink} to="/">
+          Inicio
+        </Link>
+        <p className={styles.navLink}>Búsqueda</p>
+        <p className={styles.navLink}>{capitalizeFirstLetter(params.searchInput)}</p>
+      </div>
+      <section className={styles.productsContainer}>
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product) => {
+            return <ProductCard key={product._id} product={product} />;
+          })
+        ) : (
+          <div className={styles.messageContainer}>
+            <h2 className={styles.message}>
+              Lo sentimos, no se encontraron productos que coincidan con su búsqueda.
+            </h2>
+          </div>
+        )}
+      </section>
+    </div>
   );
 };
