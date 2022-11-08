a8080ba4e529bcc72a2b93f4195f832c420368c7
FranGutierrez <francisco.gutierrez@radiumrocket.com>
2022-11-08 12:08:55 -0300

    QL-185: delete unused imports

--- src/components/pages/order/index.tsx
+++ src/components/pages/order/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import OrderLayout from 'src/components/layout/order';
 import { OrderRoutes } from 'src/constants';
