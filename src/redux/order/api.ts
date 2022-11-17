6276f0695761ad1f1dcc0c15a0533f4820fdee3c
FranGutierrez <francisco.gutierrez@radiumrocket.com>
2022-11-17 17:55:26 -0300

    QL-190: conflicts solved

--- src/redux/order/api.ts
+++
@@ -1,6 +0,0 @@
-import { api } from 'src/config/api';
-
-import { Order } from './types';
-
-export const createOrder = (order, token?: string) =>
-  api.post<Order>('/client/orders', order, { headers: { token: token } });
