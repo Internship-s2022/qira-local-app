60cbb84afa4a842368138a5bca8f2c8b671ded39
IvanHGo <ivan.gobbi@radiumrocket.com>
2022-11-18 17:43:51 -0300

    QL-291: customRender created and Login render test

---
+++ src/components/pages/login/__tests__/login.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import customRender from 'src/utils/tests/testUtils';
+
+import Login from '..';
+
+describe('Login - Unit test', () => {
+  it('should render the component correctly', () => {
+    const component = customRender(<Login />);
+  });
+});
