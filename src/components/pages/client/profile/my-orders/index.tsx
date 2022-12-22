b71f8082bfc1abe93bb7d038d9a32b1f43d0d93c
Gina Schiappa Pietra <gina.schiappapietra@radiumrocket.com>
2022-12-22 11:31:33 -0300

    QL-321: client ordersin profile sorted

--- src/components/pages/client/profile/my-orders/index.tsx
+++ src/components/pages/client/profile/my-orders/index.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import OrderCard from 'src/components/shared/ui/order-card';
 import QiraLoader from 'src/components/shared/ui/qira-loader';
 import { getClientOrders } from 'src/redux/orders/thunks';
+import { Order } from 'src/redux/orders/types';
 import { AppDispatch, RootState } from 'src/redux/store';
 
 import styles from './my-orders.module.css';
@@ -12,6 +13,15 @@ const MyOrders = (): JSX.Element => {
   const dispatch: AppDispatch<null> = useDispatch();
   const isFetching = useSelector((state: RootState) => state.orders.isFetching);
   const clientOrders = useSelector((state: RootState) => state.orders.orders);
+  const [sortedList, setSortedList] = useState<Order[]>([]);
+
+  useMemo(() => {
+    setSortedList(
+      clientOrders.sort(
+        (a, b) => new Date(b.orderDate).getTime() - new Date(a.orderDate).getTime(),
+      ),
+    );
+  }, [clientOrders]);
 
   useEffect(() => {
     dispatch(getClientOrders());
@@ -27,7 +37,7 @@ const MyOrders = (): JSX.Element => {
         <>
           <h1 className={styles.title}>Pedidos realizados</h1>
           <div className={styles.ordersContainer}>
-            {clientOrders.map((order) => (
+            {sortedList.map((order) => (
               <OrderCard key={order._id} order={order} />
             ))}
           </div>
